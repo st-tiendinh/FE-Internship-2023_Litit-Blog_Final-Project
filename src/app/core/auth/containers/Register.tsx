d1a8247f26522c0fb7d41a4c57b07e654abdb443
cmvn-linhnguyen <linh.nguyen@classmethod.vn>
2023-09-12 16:06:33 +0700

    add register page UI

--- src/app/core/auth/containers/Register.tsx
+++ src/app/core/auth/containers/Register.tsx
@@ -1,8 +1,88 @@
+import RegisterCover from '../../../../assets/images/register-cover.png';
+import { InputField } from '../../../shared/components/InputField';
+
 const Register = () => {
   return (
-    <>
-      <h1>This is register page</h1>
-    </>
+    <div className="d-flex register-page">
+      <div className="row register-wrap">
+        <div className="col col-7 register-form-wrap">
+          <form className="register-form">
+            <h2 className="register-title">Register</h2>
+            <div className="row">
+              <div className="col col-6 ">
+                <InputField label="First Name" />
+              </div>
+              <div className="col col-6 ">
+                <InputField label="Last Name" id="last-name" />
+              </div>
+              <div className="col col-12">
+                <InputField label="Email" id="email" />
+              </div>
+              <div className="col col-6">
+                <InputField label="Display name" id="display-name" />
+              </div>
+              <div className="col col-6 gender-wrap">
+                <p className="gender-label">Gender</p>
+                <ul className="radio-list d-flex">
+                  <li className="d-flex radio-item">
+                    <input
+                      id="male"
+                      name="male"
+                      type="radio"
+                      className="radio-input"
+                    />
+                    <label className="radio-label" htmlFor="male">
+                      Male
+                    </label>
+                  </li>
+                  <li className="d-flex radio-item">
+                    <input
+                      id="female"
+                      name="female"
+                      type="radio"
+                      className="radio-input"
+                    />
+                    <label className="radio-label" htmlFor="female">
+                      Female
+                    </label>
+                  </li>
+                </ul>
+              </div>
+              <div className="col col-6">
+                <InputField label="Birthday" type="date" id="birthday" />
+              </div>
+              <div className="col col-6">
+                <InputField label="Phone" type="number" id="phone" />
+              </div>
+              <div className="col col-6">
+                <InputField label="Password" type="password" id="password" />
+              </div>
+              <div className="col col-6">
+                <InputField
+                  label="Confirm Password"
+                  type="password"
+                  id="confirm-password"
+                />
+              </div>
+              <div className="col col-12 d-flex register-action">
+                <button className="btn btn-primary register-btn">
+                  Register
+                </button>
+              </div>
+            </div>
+          </form>
+          <div className="redirect-wrap">
+            <span className="redirect-message">Already have an account? </span>
+            <a className="redirect-link" href="#">
+              Login
+            </a>
+          </div>
+        </div>
+        <div className="col col-5 register-cover">
+          <img src={RegisterCover} alt="Register Cover" />
+        </div>
+      </div>
+    </div>
   );
 };
 
