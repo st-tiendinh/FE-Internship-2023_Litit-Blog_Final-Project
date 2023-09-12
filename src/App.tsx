77628a0e7cc26e793fefc774ef2db9c5658bdaec
cmvn-thaile <thai.le@classmethod.vn>
2023-09-12 15:37:57 +0700

    fix convention

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,11 @@ import { BrowserRouter } from 'react-router-dom';
 import { RouterOutlet } from './app/core/modules/custom-router-dom';
 import appRoutes from './app/app.route';
 import AppSuspense from './AppSuspense';
-import PostList from './app/shared/components/PostList';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="app">
-        <PostList/> 
         <AppSuspense fallback={<></>}>
           <RouterOutlet routes={appRoutes} />
         </AppSuspense>
