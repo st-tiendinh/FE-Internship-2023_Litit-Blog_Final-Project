f8b796db903e8ee983c779fe0a64c78a5c01e5d6
cmvn-thaile <thai.le@classmethod.vn>
2023-09-12 11:18:32 +0700

    add classname

--- src/app/shared/components/Footer.tsx
+++ src/app/shared/components/Footer.tsx
@@ -4,23 +4,24 @@ const Footer = () => {
   return (
     <footer>
       <div className="container">
-        <div className='footer'>
-        <div className="footer-header">
-          <h1>
-            <img className="footer-logo" src={logo} alt="logo" />
-          </h1>
-          <p>© JS Template 2023. All Rights Reserved.</p>
-        </div>
+        <div className="footer">
+          <div className="footer-header">
+            <h1 className="logo">
+              <img className="footer-logo" src={logo} alt="logo" />
+            </h1>
+            <p className="copyright">
+              © JS Template 2023. All Rights Reserved.
+            </p>
+          </div>
 
-        <div className="footer-content">
-          <ul className="footer-info-list">
-            <li className="footer-info-item">Terms of Use</li>
-            <li className="footer-info-item">Privacy Policy</li>
-            <li className="footer-info-item">Cookie Us</li>
-          </ul>
+          <div className="footer-content">
+            <ul className="footer-info-list">
+              <li className="footer-info-item">Terms of Use</li>
+              <li className="footer-info-item">Privacy Policy</li>
+              <li className="footer-info-item">Cookie Us</li>
+            </ul>
+          </div>
         </div>
-        </div>
-
       </div>
     </footer>
   );
