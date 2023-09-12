deeea4cd7589ae156527a175b25c7c1270b259f7
cmvn-thaile <thai.le@classmethod.vn>
2023-09-12 15:37:12 +0700

    add post list title

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { BrowserRouter } from 'react-router-dom';
 import { RouterOutlet } from './app/core/modules/custom-router-dom';
 import appRoutes from './app/app.route';
 import AppSuspense from './AppSuspense';
+import PostList from './app/shared/components/PostList';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="app">
+        <PostList/> 
         <AppSuspense fallback={<></>}>
           <RouterOutlet routes={appRoutes} />
         </AppSuspense>
--- src/app/shared/components/PostList.tsx
+++ src/app/shared/components/PostList.tsx
@@ -3,12 +3,11 @@ import Post from './Post';
 const PostList = () => {
   return (
     <div className="container ">
+      <h3 className="post-list-title">Latest Post</h3>
       <ul className="post-list row">
         <li className="post-item col col-4">
           <Post />
         </li>
-
-
       </ul>
     </div>
   );
