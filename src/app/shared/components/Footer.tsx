c25d58c50a601df0c9e70c92ef6b05e138429c9b
cmvn-thaile <thai.le@classmethod.vn>
2023-09-12 10:59:39 +0700

    create footer

---
+++ src/app/shared/components/Footer.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from '../../../assets/images/logo.png';
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className='footer'>
+        <div className="footer-header">
+          <h1>
+            <img className="footer-logo" src={logo} alt="logo" />
+          </h1>
+          <p>© JS Template 2023. All Rights Reserved.</p>
+        </div>
+
+        <div className="footer-content">
+          <ul className="footer-info-list">
+            <li className="footer-info-item">Terms of Use</li>
+            <li className="footer-info-item">Privacy Policy</li>
+            <li className="footer-info-item">Cookie Us</li>
+          </ul>
+        </div>
+        </div>
+       
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
