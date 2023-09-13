bc7b0b1775b08d241e3571e59e1f56d92cf66439
cmvn-linhnguyen <linh.nguyen@classmethod.vn>
2023-09-13 08:47:58 +0700

    add validate register form

--- src/app/core/auth/containers/Register.tsx
+++ src/app/core/auth/containers/Register.tsx
@@ -1,37 +1,104 @@
+import { useForm } from 'react-hook-form';
+
 import RegisterCover from '../../../../assets/images/register-cover.png';
-import { InputField } from '../../../shared/components/InputField';
+import { InputField } from '../../../shared/components';
+
+interface FormData {
+  firstName: string;
+  lastName: string;
+  email: string;
+  displayName: string;
+  gender: 'male' | 'female';
+  birthday: string;
+  phone: string;
+  password: string;
+  confirmPassword: string;
+}
 
 const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm<FormData>();
+
+  const onSubmit = (data: FormData) => {
+    console.log(data);
+  };
+
   return (
     <div className="d-flex register-page">
       <div className="register-wrap">
         <div className="row">
           <div className="col col-7">
             <div className="register-form-wrap">
-              <form className="register-form">
+              <form className="register-form" onSubmit={handleSubmit(onSubmit)}>
                 <h2 className="register-title">Register</h2>
                 <div className="row">
                   <div className="col col-6">
-                    <InputField label="First Name" id="first-name" />
+                    <InputField
+                      label="First Name*"
+                      id="first-name"
+                      {...register('firstName', {
+                        required: 'First name is required!',
+                      })}
+                      error={errors.firstName?.message}
+                    />
                   </div>
                   <div className="col col-6">
-                    <InputField label="Last Name" id="last-name" />
+                    <InputField
+                      label="Last Name*"
+                      id="last-name"
+                      {...register('lastName', {
+                        required: 'Last name is required!',
+                      })}
+                      error={errors.lastName?.message}
+                    />
                   </div>
                   <div className="col col-12">
-                    <InputField label="Email" id="email" />
+                    <InputField
+                      label="Email*"
+                      id="email"
+                      type="text"
+                      {...register('email', {
+                        required: 'Email is required!',
+                        pattern: {
+                          value:
+                            /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/,
+                          message: 'Email is not valid',
+                        },
+                      })}
+                      error={errors.email?.message}
+                    />
                   </div>
                   <div className="col col-6">
-                    <InputField label="Display name" id="display-name" />
+                    <InputField
+                      label="Display Name*"
+                      id="display-name"
+                      {...register('displayName', {
+                        required: 'Display name is required!',
+                        minLength: {
+                          value: 6,
+                          message:
+                            'Display name must be at least 6 characters!',
+                        },
+                      })}
+                      error={errors.displayName?.message}
+                    />
                   </div>
                   <div className="col col-6">
                     <div className="gender-wrap">
-                      <p className="gender-label">Gender</p>
+                      <p className="gender-label">Gender*</p>
                       <ul className="radio-list d-flex">
                         <li className="d-flex radio-item">
                           <input
                             id="male"
                             type="radio"
+                            value="male"
                             className="radio-input"
+                            defaultChecked
+                            {...register('gender', { required: true })}
                           />
                           <label className="radio-label" htmlFor="male">
                             Male
@@ -41,7 +108,9 @@ const Register = () => {
                           <input
                             id="female"
                             type="radio"
+                            value="female"
                             className="radio-input"
+                            {...register('gender', { required: true })}
                           />
                           <label className="radio-label" htmlFor="female">
                             Female
@@ -51,28 +120,57 @@ const Register = () => {
                     </div>
                   </div>
                   <div className="col col-6">
-                    <InputField label="Birthday" type="date" id="birthday" />
-                  </div>
-                  <div className="col col-6">
-                    <InputField label="Phone" type="number" id="phone" />
-                  </div>
-                  <div className="col col-6">
                     <InputField
-                      label="Password"
-                      type="password"
-                      id="password"
+                      label="Birthday*"
+                      type="date"
+                      id="birthday"
+                      {...register('birthday', {
+                        required: 'Birthday is required!',
+                      })}
+                      error={errors.birthday?.message}
                     />
                   </div>
                   <div className="col col-6">
                     <InputField
-                      label="Confirm Password"
+                      label="Phone*"
+                      type="tel"
+                      id="phone"
+                      {...register('phone', {
+                        required: 'Phone is required!',
+                      })}
+                      error={errors.phone?.message}
+                    />
+                  </div>
+                  <div className="col col-6">
+                    <InputField
+                      label="Password*"
+                      type="password"
+                      id="password"
+                      {...register('password', {
+                        required: 'Password is required!',
+                      })}
+                      error={errors.password?.message}
+                    />
+                  </div>
+                  <div className="col col-6">
+                    <InputField
+                      label="Confirm Password*"
                       type="password"
                       id="confirm-password"
+                      {...register('confirmPassword', {
+                        required: 'Confirm Password is required!',
+                        validate: (val: string) => {
+                          if (watch('password') != val) {
+                            return 'Your passwords do no match!';
+                          }
+                        },
+                      })}
+                      error={errors.confirmPassword?.message}
                     />
                   </div>
                   <div className="col col-12">
                     <div className="d-flex register-action">
-                      <button className="btn btn-primary register-btn">
+                      <button className="btn btn-primary" type="submit">
                         Register
                       </button>
                     </div>
--- src/app/shared/components/InputField.tsx
+++ src/app/shared/components/InputField.tsx
@@ -1,48 +1,47 @@
-import React, { InputHTMLAttributes, useState } from 'react';
+import React, { InputHTMLAttributes, useState, forwardRef } from 'react';
 
 interface InputFieldProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
   error?: string;
 }
 
-export const InputField: React.FC<InputFieldProps> = ({
-  label,
-  id,
-  name,
-  error,
-  className,
-  type,
-  ...rest
-}) => {
-  const hasError = !!error;
-  const [isShowPassword, setIsShowPassword] = useState(false);
+export const InputField = forwardRef<HTMLInputElement, InputFieldProps>(
+  ({ label, id, name, error, className, type, ...rest }, ref) => {
+    const hasError = !!error;
+    const [isShowPassword, setIsShowPassword] = useState(false);
 
-  return (
-    <div className={`input-field ${hasError ? 'error' : null}`}>
-      <div className="input-wrap">
-        <input
-          id={id}
-          name={name}
-          type={
-            type === 'password' ? (isShowPassword ? 'text' : 'password') : type
-          }
-          placeholder={label}
-          className={`input ${hasError ? 'error' : null} ${className}`}
-          {...rest}
-        />
-        <label className={`label ${hasError ? 'error' : null}`} htmlFor={id}>
-          {label}
-        </label>
-        {type === 'password' && (
-          <i
-            className={`input-icon icon ${
-              isShowPassword ? 'icon-show' : 'icon-hide'
-            }`}
-            onClick={() => setIsShowPassword(!isShowPassword)}
-          ></i>
-        )}
+    return (
+      <div className={`input-field ${hasError ? 'error' : null}`}>
+        <div className="input-wrap">
+          <input
+            ref={ref}
+            id={id}
+            name={name}
+            type={
+              type === 'password'
+                ? isShowPassword
+                  ? 'text'
+                  : 'password'
+                : type
+            }
+            placeholder={label}
+            className={`input ${hasError ? 'error' : null} ${className}`}
+            {...rest}
+          />
+          <label className={`label ${hasError ? 'error' : null}`} htmlFor={id}>
+            {label}
+          </label>
+          {type === 'password' && (
+            <i
+              className={`input-icon icon ${
+                isShowPassword ? 'icon-show' : 'icon-hide'
+              }`}
+              onClick={() => setIsShowPassword(!isShowPassword)}
+            ></i>
+          )}
+        </div>
+        <p className="error-message">{error}</p>
       </div>
-      <p className="error-message">{error}</p>
-    </div>
-  );
-};
+    );
+  }
+);
