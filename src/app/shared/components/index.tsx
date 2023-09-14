3b772d460201a00ffeaf8f0254c79b8b73c403a9
cmvn-thaile <thai.le@classmethod.vn>
2023-09-14 15:46:17 +0700

    add loading login and redirect to login after register

--- src/app/shared/components/index.tsx
+++ src/app/shared/components/index.tsx
@@ -1,5 +1,5 @@
 export { Header } from './Header';
 export { Footer } from './Footer';
 export { Post } from './Post';
-
 export { InputGroup } from './InputGroup';
+export { Spinner } from './Spinner';
