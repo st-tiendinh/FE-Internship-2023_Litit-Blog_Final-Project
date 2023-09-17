1f57af7c0eb6c7b5200a0c7c5231a57ebc7f1503
cmvn-linhnguyen <linh.nguyen@classmethod.vn>
2023-09-17 16:52:16 +0700

    add class section for list comments

--- src/app/shared/components/ListComments.tsx
+++ src/app/shared/components/ListComments.tsx
@@ -71,7 +71,7 @@ interface CommentProps {
 
 export const ListComments = () => {
   return (
-    <section className="list-comments-section">
+    <section className="section list-comments-section">
       <div className="container">
         <div className="list-comments">
           <h4 className="list-comments-title">{comments.length} comments</h4>
