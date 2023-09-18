3ff936575df94e034ae745e865a980d0aea0aa12
cmvn-linhnguyen <linh.nguyen@classmethod.vn>
2023-09-18 14:43:38 +0700

    apply api get user info

--- src/app/pages/user/containers/UserDetail.tsx
+++ src/app/pages/user/containers/UserDetail.tsx
@@ -1,9 +1,34 @@
 import { UserProfile } from './components/UserProfile';
+import { useLocation } from 'react-router-dom';
+
+import { ApiService } from '../../../core/services/api.service';
+import { useEffect, useState } from 'react';
+import { ENDPOINT } from '../../../../config/endpoint';
+import JwtHelper from '../../../core/helpers/jwtHelper';
+
+const apiService = new ApiService();
+const jwtHelper = new JwtHelper();
 
 const UserDetail = () => {
+  const [user, setUser] = useState<any>({});
+  const location = useLocation();
+  const userId = location.pathname.slice(7);
+  const isLoggedUser = jwtHelper.isCurrentUser(+userId);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await apiService.get([ENDPOINT.users.index, userId]);
+        setUser(response);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, [location]);
+
   return (
     <div className="page-user">
-      <UserProfile />
+      <UserProfile isLoggedUser={isLoggedUser} user={user} />
     </div>
   );
 };
