777cf2646c39b98dfe93e6a700136ea300086e9c
Tien Dinh N <tien.dinh@supremetech.vn>
2023-09-18 21:53:15 +0700

    Apply API for personal post list

--- src/app/pages/user/containers/components/UserPersonalPosts.tsx
+++ src/app/pages/user/containers/components/UserPersonalPosts.tsx
@@ -1,52 +1,51 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { formatDate } from '../../../../shared/utils/formatDate';
+import { isImageUrlValid } from '../../../../shared/utils/checkValidImage';
 
-export const UserPersonalPosts = () => {
+export const UserPersonalPosts = ({ userPost, postAuthor }: any) => {
+  const [isValidUserImg, setIsValidUserImg] = useState(false);
+  const formattedDate = formatDate(postAuthor?.updatedAt);
+
+  useEffect(() => {
+    isImageUrlValid(userPost).then((isValid) => {
+      isValid ? setIsValidUserImg(true) : setIsValidUserImg(false);
+    });
+  }, [isValidUserImg]);
+
   return (
     <section className="section section-personal-post">
       <ul className="personal-post-list">
-        <li className="personal-post-item">
-          <div className="personal-short-info">
-            <img
-              src="https://robohash.org/dolorummolestiaslaboriosam.png?size=50x50&set=set1"
-              className="personal-author-avatar"
-              alt=""
-            />
-            <div className="personal-author-info">
-              <p className="personal-author-name">Tiendn</p>
-              <span className="personal-post-timestamp">Sep 18</span>
-            </div>
-          </div>
-          <div className="personal-post-detail">
-            <h4 className="personal-post-title">Title</h4>
-            <Link to="" className="personal-post-comment-link">
-              <span className="btn btn-secondary">
-                <i className="icon icon-comment-darker"></i>2 comment
-              </span>
-            </Link>
-          </div>
-        </li>
-
-        <li className="personal-post-item">
-          <div className="personal-short-info">
-            <img
-              src="https://robohash.org/dolorummolestiaslaboriosam.png?size=50x50&set=set1"
-              className="personal-author-avatar"
-              alt=""
-            />
-            <div className="personal-author-info">
-              <p className="personal-author-name">Tiendn</p>
-              <span className="personal-post-timestamp">Sep 18</span>
-            </div>
-          </div>
-          <div className="personal-post-detail">
-            <h4 className="personal-post-title">Title</h4>
-            <Link to="" className="personal-post-comment-link">
-              <span className="btn btn-secondary">
-                <i className="icon icon-comment-darker"></i>2 comment
-              </span>
-            </Link>
-          </div>
-        </li>
+        {userPost.map((post: any) => {
+          return (
+            <li key={post.id} className="personal-post-item">
+              <div className="personal-short-info">
+                <img
+                  src={postAuthor?.picture}
+                  className="personal-author-avatar"
+                  alt=""
+                />
+                <div className="personal-author-info">
+                  <p className="personal-author-name">
+                    {postAuthor.displayName}
+                  </p>
+                  <span className="personal-post-timestamp">
+                    {formattedDate}
+                  </span>
+                </div>
+              </div>
+              <div className="personal-post-detail">
+                <h4 className="personal-post-title">{post.title}</h4>
+                <Link to="" className="personal-post-comment-link">
+                  <span className="btn btn-secondary">
+                    <i className="icon icon-comment-darker"></i>
+                    {post.comments} comment
+                  </span>
+                </Link>
+              </div>
+            </li>
+          );
+        })}
       </ul>
     </section>
   );
--- src/app/pages/user/containers/components/UserSidebar.tsx
+++ src/app/pages/user/containers/components/UserSidebar.tsx
@@ -1,18 +1,37 @@
-export const UserSideBar = () => {
+interface UserStatisticProps {
+  postPublicQuantity: number;
+  commentQuantity: number;
+  tagQuantity: number;
+  likeQuantity: number;
+}
+
+interface UserSideBarProps {
+  userStatistic: UserStatisticProps;
+}
+
+export const UserSideBar = ({ userStatistic }: UserSideBarProps) => {
   return (
     <aside className="user-sidebar">
       <ul className="statistic-list">
         <li className="statistic-item">
           <i className="icon icon-post-number"></i>
-          <p className="statistic-text">1 post published</p>
+          <p className="statistic-text">
+            {userStatistic.postPublicQuantity} post published
+          </p>
         </li>
         <li className="statistic-item">
           <i className="icon icon-comments-number"></i>
-          <p className="statistic-text">0 comments written</p>
+          <p className="statistic-text">
+            {userStatistic.commentQuantity} comments
+          </p>
         </li>
         <li className="statistic-item">
           <i className="icon icon-followed-tag"></i>
-          <p className="statistic-text">6 tags followed</p>
+          <p className="statistic-text">{userStatistic.tagQuantity} tags</p>
+        </li>
+        <li className="statistic-item">
+          <i className="icon icon-like-normal"></i>
+          <p className="statistic-text">{userStatistic.likeQuantity} likes</p>
         </li>
       </ul>
     </aside>
