18269cec1fd02efd7863953f792d4bde2b06a3a0
cmvn-linhnguyen <linh.nguyen@classmethod.vn>
2023-09-18 15:53:52 +0700

    add comment input ui

--- src/app/shared/components/ListComments.tsx
+++ src/app/shared/components/ListComments.tsx
@@ -1,4 +1,11 @@
+import { useSelector } from 'react-redux';
 import { Comment } from './Comment';
+import { RootState } from '../../app.reducers';
+import { useEffect, useState } from 'react';
+import { isImageUrlValid } from '../utils/checkValidImage';
+import { Link } from 'react-router-dom';
+import BlankUserImage from '../../../assets/images/blank-user.webp';
+
 export interface CommentProps {
   id: number;
   userId: number;
@@ -23,11 +30,50 @@ export interface CommentProps {
 }
 
 export const ListComments = ({ comments }: { comments: CommentProps[] }) => {
+  const [isValidUserImg, setIsValidUserImg] = useState(false);
+
+  const isLogged = useSelector(
+    (state: RootState) => state.authReducer.isLogged
+  );
+  const userInfo = useSelector(
+    (state: RootState) => state.authReducer.userInfo
+  );
+
+  useEffect(() => {
+    isImageUrlValid(userInfo.picture).then((isValid) => {
+      isValid ? setIsValidUserImg(true) : setIsValidUserImg(false);
+    });
+  }, [userInfo.picture, isValidUserImg]);
+
   return (
     <section className="section list-comments-section">
       <div className="container">
         <div className="list-comments">
           <h4 className="list-comments-title">{comments.length} comments</h4>
+          {isLogged && (
+            <div className="d-flex comment">
+              <Link className="user-link" to={''}>
+                <div className="user-avatar-wrapper">
+                  <img
+                    className="user-avatar"
+                    src={isValidUserImg ? userInfo.picture : BlankUserImage}
+                    alt="User avatar"
+                  />
+                </div>
+              </Link>
+              <form className="d-flex flex-column comment-form">
+                <textarea
+                  className="comment-input"
+                  placeholder="Enter Comment"
+                  name="comment"
+                  id="comment"
+                  autoFocus
+                  rows={3}
+                ></textarea>
+                <button className="btn btn-primary">Send</button>
+              </form>
+            </div>
+          )}
           {comments.map((item) => (
             <Comment
               key={item.id}
