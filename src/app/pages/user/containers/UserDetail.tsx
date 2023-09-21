a52825b7899ca96100ce4121b5288e664c579fc9
cmvn-thaile <thai.le@classmethod.vn>
2023-09-21 13:46:31 +0700

    add change password and soft deleted list

--- src/app/pages/user/containers/UserDetail.tsx
+++ src/app/pages/user/containers/UserDetail.tsx
@@ -1,14 +1,16 @@
-import { UserPersonalPosts } from './components/UserPersonalPosts';
-import { UserProfile } from './components/UserProfile';
-import { UserSideBar } from './components/UserSidebar';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
 
+import { UserProfile } from './components/UserProfile';
+import { UserPersonalPosts } from './components/UserPersonalPosts';
+import { UserSideBar } from './components/UserSidebar';
+import { UserChangePassword } from './components/UserChangePassword';
+
 import { ApiService } from '../../../core/services/api.service';
-import { useEffect, useState } from 'react';
+import { RootState } from '../../../app.reducers';
 import { ENDPOINT } from '../../../../config/endpoint';
 import JwtHelper from '../../../core/helpers/jwtHelper';
-import { useSelector } from 'react-redux';
-import { RootState } from '../../../app.reducers';
 
 const apiService = new ApiService();
 const jwtHelper = new JwtHelper();
@@ -18,19 +20,22 @@ export enum PostStatus {
   PRIVATE = 'private',
 }
 
+type FilterType = 'public-post' | 'deleted-post' | 'change-password';
+
 const UserDetail = () => {
   const [user, setUser] = useState<any>({});
+  const [userRecycleBin, setUserRecycleBin] = useState<any>([]);
   const [isUserLoading, setIsUserLoading] = useState<boolean>(false);
   const [userStatistic, setUserStatistic] = useState<any>({});
   const [postAuthor, setPostAuthor] = useState<any>();
   const [userPost, setUserPost] = useState<any>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const isLogged = useSelector(
-    (state: RootState) => state.authReducer.isLogged
-  );
+  const isLogged = useSelector((state: RootState) => state.authReducer.isLogged);
   const location = useLocation();
   const userId = location.pathname.slice(7);
   const isLoggedUser = isLogged ? jwtHelper.isCurrentUser(+userId) : false;
+
+  const [filter, setFilter] = useState<FilterType>('public-post');
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -51,14 +56,25 @@ const UserDetail = () => {
   }, [location]);
 
   useEffect(() => {
+    setIsUserLoading(true);
+    (async () => {
+      try {
+        apiService.setHeaders(jwtHelper.getAuthHeader());
+        const response: any = await apiService.get([ENDPOINT.posts.recyclebin]);
+        setUserRecycleBin(response.data);
+      } catch (error) {
+        console.log(error);
+        setIsUserLoading(false);
+      }
+    })();
+  }, [location]);
+
+  useEffect(() => {
     setIsLoading(true);
     (async () => {
       try {
         apiService.setHeaders(jwtHelper.getAuthHeader());
-        const response: any = await apiService.get([
-          ENDPOINT.users.index,
-          `${userId}/posts`,
-        ]);
+        const response: any = await apiService.get([ENDPOINT.users.index, `${userId}/posts`]);
 
         const postPublicQuantity = await response.Posts.filter(
           (post: any) => post.status === PostStatus.PUBLIC
@@ -116,14 +132,41 @@ const UserDetail = () => {
               )}
             </div>
             <div className="col col-8">
-              {isLoading ? (
-                <div className="skeleton skeleton-personal-list"></div>
-              ) : (
-                <UserPersonalPosts
-                  userPost={userPost}
-                  postAuthor={postAuthor}
-                />
+              {jwtHelper.getUserInfo().userId.toString() === userId && (
+                <ul className="filter">
+                  <li
+                    onClick={() => setFilter('public-post')}
+                    className={`filter-item ${filter === 'public-post' ? 'active' : ''}`}
+                  >
+                    Public posts
+                  </li>
+                  <li
+                    onClick={() => setFilter('deleted-post')}
+                    className={`filter-item ${filter === 'deleted-post' ? 'active' : ''}`}
+                  >
+                    Deleted posts
+                  </li>
+                  <li
+                    onClick={() => setFilter('change-password')}
+                    className={`filter-item ${filter === 'change-password' ? 'active' : ''}`}
+                  >
+                    Change password
+                  </li>
+                </ul>
               )}
+              {filter === 'change-password' && <UserChangePassword setFilter={setFilter} />}
+              {filter === 'deleted-post' &&
+                (isLoading ? (
+                  <div className="skeleton skeleton-personal-list"></div>
+                ) : (
+                  <UserPersonalPosts userPost={userRecycleBin} postAuthor={postAuthor} />
+                ))}
+              {filter === 'public-post' &&
+                (isLoading ? (
+                  <div className="skeleton skeleton-personal-list"></div>
+                ) : (
+                  <UserPersonalPosts userPost={userPost} postAuthor={postAuthor} />
+                ))}
             </div>
           </div>
         </section>
