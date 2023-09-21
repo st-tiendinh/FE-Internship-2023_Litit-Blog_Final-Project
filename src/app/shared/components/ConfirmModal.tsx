7c82f9833ae9575e73e6274c0d7888d941a2bacd
Tien Dinh N <tien.dinh@supremetech.vn>
2023-09-21 10:41:40 +0700

    Change folder structure

--- src/app/shared/components/ConfirmModal.tsx
+++ src/app/shared/components/ConfirmModal.tsx
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
 
 import { RootState } from '../../app.reducers';
-import { setConfirmModal, setHideModal } from '../../redux/actions/modal';
+import { setConfirmModal, setHideModal } from '../../../redux/actions/modal';
 
 export const ConfirmModal = ({ action }: any) => {
   const isShow = useSelector((state: RootState) => state.modalReducer.isShow);
