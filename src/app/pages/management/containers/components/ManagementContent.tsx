bcb9c4b921804649ae8acb40fc0d7a88e59f83b7
cmvn-linhnguyen <linh.nguyen@classmethod.vn>
2023-09-23 23:40:20 +0700

    Modify update user information and implement redux for manage user info

--- src/app/pages/management/containers/components/ManagementContent.tsx
+++ src/app/pages/management/containers/components/ManagementContent.tsx
@@ -1,5 +1,25 @@
-import React from 'react';
+import { useContext } from 'react';
+import { useSelector } from 'react-redux';
+
+import {
+  ManagementContext,
+  ManagementType,
+} from '../../../../../context/ManagementContext';
+import { UserManagement } from './UserManagement';
+
+import { RootState } from '../../../../app.reducers';
 
 export const ManagementContent = () => {
-  return <div>ManagementContent</div>;
+  const { managementType } = useContext(ManagementContext)!;
+  const userInfo = useSelector(
+    (state: RootState) => state.authReducer.userInfo
+  );
+
+  switch (managementType) {
+    case ManagementType.MY_PROFILE:
+      return <UserManagement userInfo={userInfo} />;
+
+    default:
+      return;
+  }
 };
