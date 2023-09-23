513b327517d28cf32c2657342bca1ab64c2b65e2
cmvn-linhnguyen <linh.nguyen@classmethod.vn>
2023-09-23 21:51:20 +0700

    Create Context manage filter

--- src/app/pages/management/containers/Management.tsx
+++ src/app/pages/management/containers/Management.tsx
@@ -1,18 +1,23 @@
-import React from 'react';
+import { ManagementProvider } from '../../../../context/ManagementContext';
+import { ManagementContent } from './components/ManagementContent';
 import { Sidebar } from './components/Sidebar';
 
 const Management = () => {
   return (
-    <div className="management-page">
-      <div className="container">
-        <div className="row">
-          <div className="col col-4">
-            <Sidebar />
+    <ManagementProvider>
+      <div className="management-page">
+        <div className="container">
+          <div className="row">
+            <div className="col col-4">
+              <Sidebar />
+            </div>
+            <div className="col col-8">
+              <ManagementContent />
+            </div>
           </div>
-          <div className="col col-8"></div>
         </div>
       </div>
-    </div>
+    </ManagementProvider>
   );
 };
 
--- src/app/pages/management/containers/components/Sidebar.tsx
+++ src/app/pages/management/containers/components/Sidebar.tsx
@@ -1,51 +1,63 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import {
+  ManagementContext,
+  ManagementType,
+} from '../../../../../context/ManagementContext';
 
 export const Sidebar = () => {
+  const { managementType, setManagementType } = useContext(ManagementContext)!;
+
+  const menuItems = [
+    { type: ManagementType.MY_PROFILE, label: 'Profile', icon: 'icon-profile' },
+    {
+      type: ManagementType.LIST_FOLLOWERS,
+      label: 'Followers',
+      icon: 'icon-follower',
+    },
+    {
+      type: ManagementType.LIST_FOLLOWINGS,
+      label: 'Followings',
+      icon: 'icon-following',
+    },
+    {
+      type: ManagementType.CHANGE_PASSWORD,
+      label: 'Change Password',
+      icon: 'icon-change-pw',
+    },
+    {
+      type: ManagementType.BOOKMARKS,
+      label: 'Bookmarks',
+      icon: 'icon-bookmarks',
+    },
+    { type: ManagementType.DRAFTS, label: 'Drafts', icon: 'icon-draft' },
+    {
+      type: ManagementType.RECYCLE_BIN,
+      label: 'Recycle Bin',
+      icon: 'icon-recycle-bin',
+    },
+  ];
+
+  const handleChangeManagementType = (type: ManagementType) => {
+    setManagementType(type);
+  };
+
   return (
     <div className="sidebar">
       <ul className="menu-list">
-        <li className="menu-item">
-          <div className="d-flex menu">
-            <i className="icon icon-profile"></i>
-            <span className="menu-label">Profile</span>
-          </div>
-        </li>
-        <li className="menu-item">
-          <div className="d-flex menu">
-            <i className="icon icon-follower"></i>
-            <span className="menu-label">Followers</span>
-          </div>
-        </li>
-        <li className="menu-item">
-          <div className="d-flex menu">
-            <i className="icon icon-following"></i>
-            <span className="menu-label">Followings</span>
-          </div>
-        </li>
-        <li className="menu-item">
-          <div className="d-flex menu">
-            <i className="icon icon-change-pw"></i>
-            <span className="menu-label">Change Password</span>
-          </div>
-        </li>
-        <li className="menu-item">
-          <div className="d-flex menu">
-            <i className="icon icon-bookmarks"></i>
-            <span className="menu-label">Bookmarks</span>
-          </div>
-        </li>
-        <li className="menu-item">
-          <div className="d-flex menu">
-            <i className="icon icon-draft"></i>
-            <span className="menu-label">Drafts</span>
-          </div>
-        </li>
-        <li className="menu-item">
-          <div className="d-flex menu">
-            <i className="icon icon-recycle-bin"></i>
-            <span className="menu-label">Recycle Bin</span>
-          </div>
-        </li>
+        {menuItems.map((menuItem) => (
+          <li
+            className={`menu-item ${
+              managementType === menuItem.type ? 'active' : ''
+            }`}
+            key={menuItem.type}
+            onClick={() => handleChangeManagementType(menuItem.type)}
+          >
+            <div className="d-flex menu">
+              <i className={`icon ${menuItem.icon}`}></i>
+              <span className="menu-label">{menuItem.label}</span>
+            </div>
+          </li>
+        ))}
       </ul>
     </div>
   );
