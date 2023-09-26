c82149911a505354a6bd30df718bb86db9c6185b
cmvn-linhnguyen <linh.nguyen@classmethod.vn>
2023-09-26 22:12:32 +0700

    responsive header and footer

--- src/app/shared/components/Header.tsx
+++ src/app/shared/components/Header.tsx
@@ -17,8 +17,10 @@ export const Header = () => {
   const dispatch = useDispatch();
 
   const [isValidUserImg, setIsValidUserImg] = useState(false);
+  const menuRef = useRef<HTMLElement | null>(null);
   const userActionRef = useRef<HTMLDivElement | null>(null);
   const [isOpenDropdown, setIsOpenDropdown] = useState(false);
+  const [isOpenMenu, setIsOpenMenu] = useState<boolean>(false);
   const [filter, setFilter] = useState<string | undefined>('');
 
   useEffect(() => {
@@ -64,6 +66,24 @@ export const Header = () => {
     };
   }, [setIsOpenDropdown]);
 
+  useEffect(() => {
+    const handleOutsideClick = (event: MouseEvent) => {
+      if (
+        menuRef.current &&
+        event.target instanceof Node &&
+        !menuRef.current.contains(event.target)
+      ) {
+        setIsOpenMenu(false);
+      }
+    };
+
+    window.addEventListener('click', handleOutsideClick);
+
+    return () => {
+      window.removeEventListener('click', handleOutsideClick);
+    };
+  }, [setIsOpenMenu]);
+
   const closeDropdown = () => {
     setIsOpenDropdown(false);
   };
@@ -71,143 +91,134 @@ export const Header = () => {
   return (
     <header className="header position-sticky">
       <div className="container">
-        <div className="header-inner">
-          <div className="row">
-            <div className="col col-4">
-              <div className="header-logo">
-                <Link to={'/'} className="logo-link">
-                  <h1 className="logo">
-                    <img
-                      className="logo-image"
-                      src={LogoImage}
-                      alt="Lit.it Blog"
-                    />
-                  </h1>
+        <div className="d-flex header-inner">
+          <div className="header-logo">
+            <i
+              ref={menuRef}
+              onClick={() => setIsOpenMenu(!isOpenMenu)}
+              className={`icon ${isOpenMenu ? 'icon-close' : 'icon-menu'}`}
+            ></i>
+            <Link to={'/'} className="logo-link">
+              <h1 className="logo">
+                <img className="logo-image" src={LogoImage} alt="Lit.it Blog" />
+              </h1>
+            </Link>
+          </div>
+          <nav className={`nav ${isOpenMenu ? 'active' : null}`}>
+            <ul className="d-flex nav-list">
+              <li className="nav-item">
+                <Link
+                  to={'/'}
+                  className={`nav-link ${filter === '' ? 'active' : null}`}
+                >
+                  Home
                 </Link>
-              </div>
-            </div>
-            <div className="col col-4">
-              <nav className="nav">
-                <ul className="d-flex nav-list">
-                  <li className="nav-item">
-                    <Link
-                      to={'/'}
-                      className={`nav-link ${filter === '' ? 'active' : null}`}
-                    >
-                      Home
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link
-                      to={'articles'}
-                      className={`nav-link ${
-                        filter === 'articles' ? 'active' : null
-                      }`}
-                    >
-                      Articles
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link
-                      to={'/articles/new'}
-                      className={`nav-link ${
-                        filter === 'new' ? 'active' : null
-                      }`}
-                    >
-                      Write
-                    </Link>
-                  </li>
-                </ul>
-              </nav>
-            </div>
-            <div className="col col-4">
-              <div className="d-flex header-action">
-                <ul className="d-flex action-list">
-                  {isLogged && (
-                    <li className="action-item">
-                      <div
-                        onClick={() => setIsOpenDropdown(!isOpenDropdown)}
-                        ref={userActionRef}
-                        className={`user-action ${
-                          isOpenDropdown ? 'active' : null
-                        }`}
-                      >
-                        <p className="user-name">{userInfo.displayName}</p>
-                      </div>
-                      {isOpenDropdown && (
-                        <div className="dropdown-menu">
-                          <ul className="menu-list">
-                            <li className="menu-item">
-                              <Link
-                                className="d-flex menu-action"
-                                to={`users/${jwtHelper.getUserInfo().userId}`}
-                              >
-                                <div className="user-avatar-wrapper">
-                                  <img
-                                    className="user-avatar"
-                                    src={
-                                      isValidUserImg
-                                        ? userInfo.picture
-                                        : BlankUserImg
-                                    }
-                                    alt="User Image"
-                                  />
-                                </div>
-                                <div className="user-info">
-                                  <p className="user-name text-truncate">
-                                    {userInfo.displayName}
-                                  </p>
-                                  <p className="user-email text-truncate">
-                                    {userInfo.email}
-                                  </p>
-                                </div>
-                              </Link>
-                            </li>
-                            <Link
-                              to={'/management'}
-                              className="menu-item"
-                              onClick={closeDropdown}
-                            >
-                              <div className="menu-action">Management</div>
-                            </Link>
-                            <li className="menu-item">
-                              <div
-                                onClick={handleSignOut}
-                                className="menu-action action-logout"
-                              >
-                                <p className="logout-label">Sign out</p>
-                              </div>
-                            </li>
-                          </ul>
-                        </div>
-                      )}
-                    </li>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to={'articles'}
+                  className={`nav-link ${
+                    filter === 'articles' ? 'active' : null
+                  }`}
+                >
+                  Articles
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to={'/articles/new'}
+                  className={`nav-link ${filter === 'new' ? 'active' : null}`}
+                >
+                  Write
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          <div className="d-flex header-action">
+            <ul className="d-flex action-list">
+              {isLogged && (
+                <li className="action-item">
+                  <div
+                    onClick={() => setIsOpenDropdown(!isOpenDropdown)}
+                    ref={userActionRef}
+                    className={`user-action ${
+                      isOpenDropdown ? 'active' : null
+                    }`}
+                  >
+                    <p className="user-name">{userInfo.displayName}</p>
+                  </div>
+                  {isOpenDropdown && (
+                    <div className="dropdown-menu">
+                      <ul className="menu-list">
+                        <li className="menu-item">
+                          <Link
+                            className="d-flex menu-action"
+                            to={`users/${jwtHelper.getUserInfo().userId}`}
+                          >
+                            <div className="user-avatar-wrapper">
+                              <img
+                                className="user-avatar"
+                                src={
+                                  isValidUserImg
+                                    ? userInfo.picture
+                                    : BlankUserImg
+                                }
+                                alt="User Image"
+                              />
+                            </div>
+                            <div className="user-info">
+                              <p className="user-name text-truncate">
+                                {userInfo.displayName}
+                              </p>
+                              <p className="user-email text-truncate">
+                                {userInfo.email}
+                              </p>
+                            </div>
+                          </Link>
+                        </li>
+                        <Link
+                          to={'/management'}
+                          className="menu-item"
+                          onClick={closeDropdown}
+                        >
+                          <div className="menu-action">Management</div>
+                        </Link>
+                        <li className="menu-item">
+                          <div
+                            onClick={handleSignOut}
+                            className="menu-action action-logout"
+                          >
+                            <p className="logout-label">Sign out</p>
+                          </div>
+                        </li>
+                      </ul>
+                    </div>
                   )}
+                </li>
+              )}
 
-                  {location.pathname === '/auth/login' && !isLogged && (
-                    <li className="action-item">
-                      <Link
-                        to={'auth/register'}
-                        className="btn btn-secondary action-link"
-                      >
-                        Sign up
-                      </Link>
-                    </li>
-                  )}
+              {location.pathname === '/auth/login' && !isLogged && (
+                <li className="action-item">
+                  <Link
+                    to={'auth/register'}
+                    className="btn btn-secondary action-link"
+                  >
+                    Sign up
+                  </Link>
+                </li>
+              )}
 
-                  {!isLogged && location.pathname !== '/auth/login' && (
-                    <li className="action-item">
-                      <Link
-                        to={'auth/login'}
-                        className="btn btn-secondary action-link"
-                      >
-                        Sign in
-                      </Link>
-                    </li>
-                  )}
-                </ul>
-              </div>
-            </div>
+              {!isLogged && location.pathname !== '/auth/login' && (
+                <li className="action-item">
+                  <Link
+                    to={'auth/login'}
+                    className="btn btn-secondary action-link"
+                  >
+                    Sign in
+                  </Link>
+                </li>
+              )}
+            </ul>
           </div>
         </div>
       </div>
