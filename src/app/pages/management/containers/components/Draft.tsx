e1be5c57d965060cdd7af5db3124db70d0570e6d
Tien Dinh N <tien.dinh@supremetech.vn>
2023-09-26 18:58:57 +0700

    Add loadmore button for draft list

--- src/app/pages/management/containers/components/Draft.tsx
+++ src/app/pages/management/containers/components/Draft.tsx
@@ -12,7 +12,11 @@ const apiService = new ApiService();
 const jwtHelper = new JwtHelper();
 
 export const Draft = () => {
-  const [draftList, setDraftList] = useState<any>([]);
+  const [draftList, setDraftList] = useState<any[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [page, setPage] = useState(1);
+  const [visiblePosts, setVisiblePosts] = useState<any[]>([]);
+
   const id = useSelector((state: RootState) => state.modalReducer.id);
   const userInfo = useSelector(
     (state: RootState) => state.authReducer.userInfo
@@ -21,17 +25,42 @@ export const Draft = () => {
     (state: RootState) => state.modalReducer.isConfirm
   );
 
+  const handleLoadMore = () => {
+    const startIndex = page * 5;
+    let endIndex = startIndex + 5;
+    if (endIndex > draftList.length) {
+      endIndex = startIndex + (Number(draftList.length) - Number(startIndex));
+    }
+    const newPosts = draftList.slice(startIndex, endIndex);
+    setVisiblePosts((prevPosts) => [...prevPosts, ...newPosts]);
+    setPage((prevPage) => prevPage + 1);
+  };
+
+  useEffect(() => {
+    if (draftList.length >= 0) {
+      setVisiblePosts(draftList.slice(0, 5));
+    }
+  }, [draftList]);
+
   useEffect(() => {
     (async () => {
-      apiService.setHeaders(jwtHelper.getAuthHeader());
-      const res: any = await apiService.get([ENDPOINT.posts.draft]);
-      const draftData = res.map((post: IPost) => {
-        return { ...post, user: userInfo };
-      });
-      const sortedDraft = draftData.sort((a: IPost, b: IPost) => {
-        return (new Date(b.createdAt) as any) - (new Date(a.createdAt) as any);
-      });
-      setDraftList(sortedDraft);
+      try {
+        setIsLoading(true);
+        apiService.setHeaders(jwtHelper.getAuthHeader());
+        const res: any = await apiService.get([ENDPOINT.posts.draft]);
+        const draftData = res.map((post: IPost) => {
+          return { ...post, user: userInfo };
+        });
+        const sortedDraft = draftData.sort((a: IPost, b: IPost) => {
+          return (
+            (new Date(b.createdAt) as any) - (new Date(a.createdAt) as any)
+          );
+        });
+        setDraftList(sortedDraft);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
     })();
   }, []);
 
@@ -45,7 +74,22 @@ export const Draft = () => {
 
   return (
     <section className="section section-draft">
-      <PostList posts={draftList} type={PostListType.LIST} isHasAction={true} />
+      {isLoading ? (
+        <div className="skeleton skeleton-personal-list"></div>
+      ) : (
+        <PostList
+          posts={visiblePosts}
+          type={PostListType.LIST}
+          isHasAction={true}
+        />
+      )}
+      <div className="d-flex load-more-btn-wrap">
+        {visiblePosts.length < draftList.length && (
+          <button className="btn btn-outline" onClick={handleLoadMore}>
+            Load More
+          </button>
+        )}
+      </div>
     </section>
   );
 };
